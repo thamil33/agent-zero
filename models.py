f23a77c459dd8ca6b8570f2c2d5acbfd47d77b50
thamil33 <tyler.hamilton4814@outlook.com>
2025-08-24 10:03:59 -0400

    major conversion
    Scrai

--- models.py
+++ models.py
@@ -528,8 +528,8 @@ def _adjust_call_args(provider_name: str, model_name: str, kwargs: dict):
     # for openrouter add app reference
     if provider_name == "openrouter":
         kwargs["extra_headers"] = {
-            "HTTP-Referer": "https://agent-zero.ai",
-            "X-Title": "Agent Zero",
+            "HTTP-Referer": "https://github.com/thamil33",
+            "X-Title": "ScrAI",
         }
 
     # remap other to openai for litellm
